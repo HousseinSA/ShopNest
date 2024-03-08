d706ec5617663721b7385b49ddfc16d09dca198b
houssein sedoum <nejihoussein1@gmail.com>
2024-03-08 23:21:04 +0000

    intial commit ShopNest

---
+++ app/(route)/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const page = () => {
+  return (
+    <div className="p-4">
+      <Button variant={'outline'}>click me</Button>
+    </div>
+  );
+};
+
+export default page;
