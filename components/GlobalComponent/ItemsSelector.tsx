aec69e7c5dec5f8f114312b4038d1bff4e6db17f
houssein sedoum <nejihoussein1@gmail.com>
2024-04-01 01:52:34 +0000

    fixing product form

--- components/GlobalComponent/ItemsSelector.tsx
+++ components/GlobalComponent/ItemsSelector.tsx
@@ -5,70 +5,53 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Billboard, Category, Color, Size } from '@prisma/client'
 import { boolean } from 'zod'
 
-interface BillboardProps {
+// Define a base interface for shared props
+interface BaseProps {
   disabled: boolean
   valueChange: () => void
-  value: string
+  value: string | boolean
   defaultValue: string
+}
+
+// Extend the base interface for each specific prop set
+interface BillboardProps extends BaseProps {
   items: Billboard[]
   itemType: 'billboard'
 }
 
-interface SizesProps {
-  disabled: boolean
-  valueChange: () => void
-  value: string
-  defaultValue: string
+interface SizesProps extends BaseProps {
   items: Size[]
   itemType: 'size'
 }
-interface CategoryProps {
-  disabled: boolean
-  valueChange: () => void
-  value: string
-  defaultValue: string
+
+interface CategoryProps extends BaseProps {
   items: Category[]
   itemType: 'category'
 }
-interface ColorProps {
-  disabled: boolean
-  valueChange: () => void
-  value: string
-  defaultValue: string
+
+interface ColorProps extends BaseProps {
   items: Color[]
   itemType: 'color'
 }
-interface ProductState {
-  disabled: boolean
-  valueChange: () => void
-  value: boolean
-  defaultValue: string
-  items: boolean
-  itemType: 'state'
-}
-type GlobalProps = BillboardProps | SizesProps | CategoryProps | ColorProps | ProductState
+
+type itemProps = Billboard | Size | Category | Color
+// Create a union type for all props
+type GlobalProps = BillboardProps | SizesProps | CategoryProps | ColorProps 
 
 const ItemsSelector: React.FC<GlobalProps> = ({ disabled, valueChange, value, defaultValue, items, itemType }) => {
   return (
-    <Select onValueChange={valueChange} value={value} disabled={disabled}>
+    <Select onValueChange={valueChange} value={value as string} disabled={disabled}>
       <SelectTrigger className='w-[180px]'>
         <SelectValue defaultValue={defaultValue} placeholder={` Select ${itemType}`} />
       </SelectTrigger>
       <SelectContent>
-        {Array.isArray(items) ? (
-          items.map((item) => {
-            return (
-              <SelectItem key={item.id} value={item.id}>
-                {itemType === 'size' || itemType === 'category' || itemType === 'color' ? (item as Size).name : (item as Billboard).label}
-              </SelectItem>
-            )
-          })
-        ) : (
-          <>
-            <SelectItem value={true}>yes</SelectItem>
-            <SelectItem value={false}>no</SelectItem>
-          </>
-        )}
+        {items.map((item: itemProps) => {
+          return (
+            <SelectItem key={item.id} value={item.id}>
+              {itemType === 'size' || itemType === 'category' || itemType === 'color' ? (item as Size).name : (item as Billboard).label}
+            </SelectItem>
+          )
+        })}
       </SelectContent>
     </Select>
   )
