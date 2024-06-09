3a7e6e347f210e34a4ee7baabd47b70ed24ea84d
hussein <husseinsedoum@gmail.com>
2024-06-09 02:02:45 +0000

    fixing logo

--- ShopNest-Frontend/components/layouts/Header.tsx
+++ ShopNest-Frontend/components/layouts/Header.tsx
@@ -1,32 +1,33 @@
-import Image from "next/image"
-import Link from "next/link"
-import React from "react"
-import Navigation from "./Navigation"
-import getCategoriesData from "@/lib/fetchData/getCategories"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Navigation from "./Navigation";
+import getCategoriesData from "@/lib/fetchData/getCategories";
 
-export const revalidate  = 0 
+export const revalidate = 0;
 const Header = async () => {
-  
-    const categories = await getCategoriesData()
+  const categories = await getCategoriesData();
+  console.log(categories)
   return (
     <header className="mx-auto py-2 md:py-3 lg:py-5 max-w-7xl border-b">
       <div className="flex items-center justify-between">
         <div className="w-auto h-auto">
           <Link href={"/"}>
             <Image
-              src={"/shopnest-high-resolution-logo-transparent.png"}
-              width={100}
-              height={100}
               alt="logo"
+              loading="lazy"
+              width="200"
+              height="150"
+              decoding="async"
+              data-nimg="1"
+              src="/_next/image?url=%2Fshopnest-high-resolution-logo-transparent.png&w=640&q=75"
             />
           </Link>
         </div>
         <Navigation categoriesData={categories} />
       </div>
     </header>
-  
-  
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
