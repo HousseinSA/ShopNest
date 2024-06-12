9c7ad61dbea56a4747249761e1ca7e519e063b67
hussein <husseinsedoum@gmail.com>
2024-06-12 02:43:02 +0000

    more improvement  and created the iproduct pages in frontend

--- ShopNest-Dashboard/components/GlobalComponent/ImageUpload.tsx
+++ ShopNest-Dashboard/components/GlobalComponent/ImageUpload.tsx
@@ -32,9 +32,9 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
 
   return (
     <div>
-      <div className='flex items-center gap-4 my-4'>
+      <div className='flex flex-wrap gap-4 my-4'>
         {value.map((url) => (
-          <div key={url} className='relative w-[250px] h-[200px] flex items-center justify-center bg-gray-100 rounded-md overflow-hidden'>
+          <div key={url} className='relative w-[250px] h-[250px] flex items-center justify-center bg-gray-100 rounded-md overflow-hidden'>
             <div className='absolute top-2 right-2 z-10'>
               <Button
                 type='button'
@@ -47,10 +47,10 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
             </div>
             <Image
               src={url}
-              alt='Image'
-              layout='fill'
-              objectFit='fill'
-              className='w-full h-full'
+              alt={'uploaded image'}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
             />
           </div>
         ))}
