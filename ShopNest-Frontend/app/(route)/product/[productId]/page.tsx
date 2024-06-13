d7f2165472c99345d438c847c5130fed8a713e8a
hussein <husseinsedoum@gmail.com>
2024-06-13 01:05:52 +0000

    adding more stuff into product page

--- ShopNest-Frontend/app/(route)/product/[productId]/page.tsx
+++ ShopNest-Frontend/app/(route)/product/[productId]/page.tsx
@@ -2,29 +2,25 @@ import getProducts from "@/lib/fetchData/getProducts";
 import getProduct from "@/lib/fetchData/getProduct";
 import Container from "@/components/ui/container";
 import Products from "../../../../components/products/products";
-import Gallery from "@/components/products/product/gallery";
-
+import Gallery from "@/components/products/product/galleryComponent/gallery";
+import Info from "@/components/products/product/info";
 
 const ProductPage = async ({ params }: { params: { productId: string } }) => {
   const product = await getProduct(params.productId);
   const productByCategory = await getProducts({
     categoryCode: product.category.id,
   });
-
   return (
-    <div className="bg-white">  
-      <Container>
-      <div className="px-4 py-10 sm:px-6 lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-     {/* Gallery */}
-     <Gallery images ={product.images}/>
-          <div className="mt-10 sm:mt-16 px-4 sm:px-0 lg:mt-0">info</div>
+    <Container>
+      <div className="grid grid-cols-1  lg:grid-cols-2 lg:gap-8">
+        <Gallery  images={product.images} />
+        <div className="mt-10 px-4 md:mt-16 lg:mt-0">
+          <Info product={product}/>
         </div>
       </div>
-      </Container>
-      <hr className="py-10" />
-      <Products title="Related products " products={productByCategory}/>
-    </div>
+      <hr className="my-10" />
+      <Products title="Related products" products={productByCategory} />
+    </Container>
   );
 };
 
