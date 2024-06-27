35bc8131c1f7bbde26e15388ce7a5bc0df464df8
hussein <husseinsedoum@gmail.com>
2024-06-27 23:06:47 +0000

    more improvemnt into project

--- ShopNest-Dashboard/app/(dashboard)/[storeCode]/page.tsx
+++ ShopNest-Dashboard/app/(dashboard)/[storeCode]/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { redirect } from 'next/navigation'
 import { auth } from '@clerk/nextjs'
-import validateObjectId from '@/lib/mongodDBValidate'
+import { CreditCard, DollarSign, Package } from 'lucide-react'
 
-import prismaDB from '@/lib/prismaClient'
+
+import SectionHeader from '@/components/globals/storeHead/SectionHeader'
+import { Separator } from '@/components/ui/separator'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { PriceFormatter } from '@/lib/PriceFormatter'
+import { getTotalRevenue } from '@/lib/actions/getTotalRevenue'
+import { getSalesCount } from '@/lib/actions/getSalesCount'
+import { getInStockCount } from '@/lib/actions/getInStockCount'
+import validateObjectId from '@/lib/mongodDBValidate'
+import Overview from '@/components/Dashboard/overview'
+import { getGraphData } from '@/lib/actions/getGraphData'
 interface StoreParams {
   params: { storeCode: string }
 }
@@ -17,14 +27,63 @@ const StorePage: React.FC<StoreParams> = async ({ params }: { params: { storeCod
   if (!validStoreCode) {
     redirect(`/`)
   }
-  const store = await prismaDB.store.findFirst({
-    where: {
-      id: params.storeCode,
-      userId
-    }
-  })
+  // const store = await prismaDB.store.findFirst({
+  //   where: {
+  //     id: params.storeCode,
+  //     userId
+  //   }
+  // })
 
-  return <div>active store:{store?.storeName}</div>
+  const totalRevenue = await getTotalRevenue(params.storeCode)
+  const salesCount = await getSalesCount(params.storeCode)
+  const stockCount = await getInStockCount(params.storeCode)
+  const graphData = await getGraphData(params.storeCode)
+
+  return (
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <SectionHeader title='Dashboard' description='Your store overview ' />
+        <Separator />
+        <div className='grid gap-4 grid-cols-3'>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium text-primary'>Total Revenue</CardTitle>
+              <DollarSign className='w-4 h-4 text-muted-foreground  ' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>{PriceFormatter.format(totalRevenue)}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium text-primary'>Sales</CardTitle>
+              <CreditCard className='w-4 h-4 text-muted-foreground  ' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>{salesCount}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium  text-primary'>Products in stuck</CardTitle>
+              <Package className='w-4 h-4 text-muted-foreground  ' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>{stockCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className='col-span-4'>
+          <CardHeader>
+            <CardTitle className=' text-primary'>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className='pl-2'>
+            <Overview data={graphData}/>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default StorePage
---
+++ ShopNest-Dashboard/components/Dashboard/overview.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import OnlyClient from '../globals/OnlyClient'
+
+interface OverViewProps {
+  data: any[]
+}
+const Overview :React.FC<OverViewProps>= ({data}) => {
+  
+  return (
+
+    <OnlyClient>
+    <ResponsiveContainer width='100%' height={350}>
+      <BarChart data={data}>
+        <XAxis dataKey={'name'} stroke='#888888' fontSize={12} tickLine={false} axisLine={false} />
+        <YAxis stroke='#888888' fontSize={12} tickLine={false} axisLine={false} tickFormatter={(value) => `$${value}`} />
+            <Bar dataKey='total' fill='#16A34A'   radius={[4, 4, 0, 0 ]}/>
+      </BarChart>
+    </ResponsiveContainer>
+    </OnlyClient>
+  )
+}
+
+export default Overview
