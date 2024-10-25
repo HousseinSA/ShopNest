3af22ef3f0aa753c0b50ffbd26479ccac341590b
hussein <husseinsedoum@gmail.com>
2024-10-25 11:54:09 +0000

    fixing issue

--- ShopNest-Dashboard/app/(dashboard)/[storeCode]/layout.tsx
+++ ShopNest-Dashboard/app/(dashboard)/[storeCode]/layout.tsx
@@ -20,6 +20,9 @@ export default async function DashboardLayout({ children, params: { storeCode } 
 if(userId && storeList.length === 0){
   storeList = await prismaDB.store.findMany({ where: { userId:'guest' } });
 }
+
+    console.log('getting user info',userId)
+    
   return (
     <div className='max-w-7xl mx-auto'>
       {!userId && <NotRegisteredUser/>}
